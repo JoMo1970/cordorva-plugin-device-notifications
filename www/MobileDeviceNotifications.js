728d8c5addb1dc7f3d7f7e6e1d1375b5bbee8093
John Moline <albaresapps@gmail.com>
2017-03-26 00:16:19 -0500

    updated passed in js arguments

--- www/MobileDeviceNotifications.js
+++ www/MobileDeviceNotifications.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 //this cordova function interacts with the native app
-exports.sendNotification= function(arg0, success, error) {
-    exec(success, error, "MobileDeviceNotifications", "InvokeDeviceNotification", [arg0]);
+exports.sendNotification= function(args, success, error) {
+    exec(success, error, "MobileDeviceNotifications", "InvokeDeviceNotification", args);
 };
 
 /*exports.coolMethod= function(arg0, success, error) {
